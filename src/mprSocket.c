ea3617a54ffcb7d0786c316eca329c3abd0a24a4
Embedthis Software <dev@embedthis.com>
2012-10-17 22:26:25 -0700

    Verify peer SSL certificates by default

--- src/mprSocket.c
+++ src/mprSocket.c
@@ -1550,8 +1550,8 @@ PUBLIC MprSsl *mprCreateSsl()
     ssl->ciphers = sclone(BIT_CIPHERS);
     ssl->protocols = MPR_PROTO_TLSV1 | MPR_PROTO_TLSV11;
     ssl->verifyDepth = 1;
-    ssl->verifyPeer = 0;
-    ssl->verifyIssuer = 0;
+    ssl->verifyPeer = 1;
+    ssl->verifyIssuer = 1;
     return ssl;
 }
 
