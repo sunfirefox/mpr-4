55f43b3678adf0f30d88e9b0316d975d48ad5d21
Embedthis Software <dev@embedthis.com>
2013-03-28 17:06:22 -0700

    Test better windows make clean

--- projects/mpr-windows-default-bit.h
+++ projects/mpr-windows-default-bit.h
@@ -149,7 +149,7 @@
 
 /* Profile */
 #ifndef BIT_CONFIG_CMD
-    #define BIT_CONFIG_CMD "bit -d -q -platform windows-x86-default -configure . -gen nmake"
+    #define BIT_CONFIG_CMD "bit -d -q -platform windows-x86-default -configure . -gen vs"
 #endif
 #ifndef BIT_MPR_PRODUCT
     #define BIT_MPR_PRODUCT 1
