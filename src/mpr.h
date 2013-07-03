765ad8fd33f5fa8e47c1827f6f686a7342a64237
Embedthis Software <dev@embedthis.com>
2013-07-03 10:56:40 -0700

    Regen doc

--- src/mpr.h
+++ src/mpr.h
@@ -4125,7 +4125,7 @@ PUBLIC MprHash *mprBlendHash(MprHash *target, MprHash *other);
 
 /**
     Convert a hash of strings to a single string
-    @param list List pointer returned from mprCreateList.
+    @param hash Hash pointer returned from mprCreateHash.
     @param join String to use as the element join string.
     @return String consisting of the joined hash values
     @ingroup MprHash
@@ -4135,7 +4135,7 @@ PUBLIC char *mprHashToString(MprHash *hash, cchar *join);
 
 /**
     Convert hash keys to a single string
-    @param list List pointer returned from mprCreateList.
+    @param hash Hash pointer returned from mprCreateHash.
     @param join String to use as the element join string.
     @return String consisting of the joined hash keys
     @ingroup MprHash
